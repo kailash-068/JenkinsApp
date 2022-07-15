1aafada12f7bc6fb2b7ecf721c0892c3a7a4ea2e
kailash-068 <kailashthalore1998@gmail.com>
2022-07-15 14:31:05 +0530

    Commit new line

--- JenkinsApp/Program.cs
+++ JenkinsApp/Program.cs
@@ -8,6 +8,7 @@ namespace JenkinsApp
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Welcome to Jenkins !");
+            Console.WriteLine("This is the first use of Jenkins");
             Console.ReadLine();
         }
     }
