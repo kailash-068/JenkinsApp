b149c161bd33373b4e5c6f6d20301fbfc6503a03
kailash-068 <kailashthalore1998@gmail.com>
2022-07-15 16:25:37 +0530

    Save changes.

--- JenkinsApp/Program.cs
+++ JenkinsApp/Program.cs
@@ -10,9 +10,11 @@ namespace JenkinsApp
             Console.WriteLine("Hello World!");
             Console.WriteLine("Welcome to Jenkins !");
             Console.WriteLine("This is the first use of Jenkins");
+            Console.WriteLine("      ");
             Console.WriteLine("---***---");
+            Console.WriteLine("      ");
             MyMaths myMaths = new MyMaths();
-            int x = myMaths.add(4, 5);
+            int x = myMaths.add(5, 6);
             int y = myMaths.multiply(4, 5);
             Console.WriteLine($"{x} ------ {y}");
             Console.ReadLine();
