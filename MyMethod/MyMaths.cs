4b1addf25d8dacac8cc8755eb09a38bad4db4b8f
kailash-068 <kailashthalore1998@gmail.com>
2022-07-15 16:20:17 +0530

    MyMaths file commit

---
+++ MyMethod/MyMaths.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyMethod
+{
+    public class MyMaths
+    {
+        public int add(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+        public int multiply(int a, int b)
+        {
+            int multiply = a * b;
+            return multiply;
+        }
+    }
+}
